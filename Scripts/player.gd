63832a868b2293235338c911f7cbab915645572d
beltranbot <beltranbot@gmail.com>
2025-03-07 22:10:49 -0500

    8. player 4

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,12 +10,15 @@ var facing_angle: float
 
 @onready var model: MeshInstance3D = get_node("Model")
 
-# private vars
+#### private vars ####
 var _input
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
 
 	_input = Input.get_vector(
 		"move_left",
@@ -36,10 +39,19 @@ func _physics_process(delta: float) -> void:
 
 	_model_facing_angle()
 
+	if global_position.y < -5:
+		_game_over()
 
-func _model_facing_angle():
+
+#### private ####
+
+func _model_facing_angle() -> void:
 	if _input.length() > 0:
 		facing_angle = Vector2(_input.y, _input.x).angle()
 
 	model.rotation.y = lerp_angle( \
 		model.rotation.y, facing_angle, rotation_speed)
+
+
+func _game_over() -> void:
+	get_tree().call_deferred("reload_current_scene")
