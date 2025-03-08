b22337aba88df26906f1cdab16755d1461f35519
beltranbot <beltranbot@gmail.com>
2025-03-07 22:36:24 -0500

    enemy part 1

---
+++ Scripts/enemy.gd
@@ -0,0 +1,26 @@
+extends Area3D
+
+
+@export var move_speed: float = 2.0
+@export var move_dir: Vector3
+
+var start_pos: Vector3
+var target_pos: Vector3
+
+
+func _ready():
+	start_pos = global_position
+	target_pos = start_pos + move_dir
+
+
+func _process(delta: float) -> void:
+	global_position = global_position.move_toward(
+		target_pos,
+		move_speed * delta
+	)
+
+	if global_position == target_pos:
+		if global_position == start_pos:
+			target_pos = start_pos + move_dir
+		else:
+			target_pos = start_pos
